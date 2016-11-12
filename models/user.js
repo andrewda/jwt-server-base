33181b4002fd854e7ffbc501834d4002e85153b2
Andrew Dassonville <dassonville.andrew@gmail.com>
2016-11-12 10:35:25 -0800

    Stop using this

--- models/user.js
+++ models/user.js
@@ -28,7 +28,7 @@ userSchema.pre('save', function(next) {
 userSchema.methods.comparePassword = function(candidatePassword, callback) {
 	const user = this;
 
-	bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+	bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
 		if (err) { return callback(err); }
 
 		callback(null, isMatch);
